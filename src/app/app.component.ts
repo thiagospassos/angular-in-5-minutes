0662ac390d199ec210aebcb47d04f26a57b29216
Thiago Passos <thiago@passos.com.au>
2018-05-17 12:01:53 +1000

    sample data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello World';
+
+  update(){
+    this.title = "Angular In 5 Minutes";
+  }
 }
