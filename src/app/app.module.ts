d0992d7320b75163a015adbeb382de914a2a26c6
Thiago Passos <thiago@passos.com.au>
2018-05-31 17:23:36 +1000

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
@@ -18,7 +19,12 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'contact', component: ContactComponent }
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
