1485547f819ce80c41dee0aab2ba53327aefa5d8
Thiago Passos <thiago@passos.com.au>
2018-05-31 17:39:20 +1000

    bhaskara's formula

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  a: number;
+  b: number;
+  c: number;
+
+  result: number;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getResult() {
+    this.result = this.calculate(this.a, this.b, this.c);
+  }
+
+  calculate(a: number, b: number, c: number) {
+    if (a == 0) return 0;
+    let delta = Math.sqrt(Math.abs(Math.pow(b, 2) - (4 * a * c)));
+    return (-b + delta) / (2 * a);
   }
 
 }
